60a89e8dd58c99eafc02d4dbc03cb9777c2d8863
WDavid Calsin <david2c142@gmail.com>
2021-08-31 11:47:01 -0500

    feat: 🎸 size limit for textarea (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <h2 className="text-secondary font-semibold text-xl font-noto-sans">Nueva Tarea</h2>
         <form action="">
           <input placeholder="Title" className="appearance-none border-b-2 border-secondary font-noto-sans w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2 mb-2" type="text" />
-          <textarea placeholder="Description ..." className="w-full h-40 resize border border-secondary rounded-md focus:outline-none  py-2 px-3 mt-2 mb-2 font-noto-sans"></textarea>
+          <textarea placeholder="Description ..." className="w-full max-w-full h-40 resize border border-secondary rounded-md focus:outline-none  py-2 px-3 mt-2 mb-2 font-noto-sans"></textarea>
           <button className=" block bg-primary hover:bg-blue-700 text-white font-noto-sans py-2 px-4 rounded-sm mr-0 m-auto">Create Task</button>
         </form>
       </div>
