e0f041742bcbabdf8637cb8313849e443e8352d4
WDavid Calsin <david2c142@gmail.com>
2021-08-31 23:54:24 -0500

    feat: 🎸 added to-do lists and the modal (#3)

--- src/page/list-tasks.tsx
+++ src/page/list-tasks.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Layout from '../components/layout'
+import ModalTask from "../components/modalTasks";
 
 function ListTasks() {
     return (
         <Layout>
-            <div
+            {/* <div
                 className="
                     h-auto
                     mx-4
@@ -18,6 +19,13 @@ function ListTasks() {
                     "
             >
                 Lista 1
+            </div> */}
+            <div className="sm:w-3/6 w-full flex flex-col space-y-2">
+
+                <ModalTask />
+                <ModalTask />
+                <ModalTask />
+
             </div>
         </Layout>
 
