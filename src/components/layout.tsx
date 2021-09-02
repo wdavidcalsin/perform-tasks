a4933669ab0868eaa227a678b0c9e1f50c72c56c
WDavid Calsin <wdavidcalsin968@gmail.com>
2021-09-01 22:34:34 -0500

    refactor: 💡 added react-hook-form

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React from 'react';
 import lineasImg from '../image/lineas-image-task-project.png';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 type Props = {
-    children: JSX.Element,
+    children: JSX.Element;
 };
 
 function Layout({ children }: Props) {
     return (
         <div className="h-screen w-full flex items-center flex-col">
-
             <div className="top-0 right-0 h-screen flex w-full absolute gradient-radial-to-tr -z-10 from-primary to-secondary">
                 <div className="w-1/2 h-screen bg-secondary"></div>
                 <div className="w-1/2 h-screen bg-primary"></div>
-                <img src={lineasImg} className=" w-full absolute bottom-0 right-0" alt="" />
+                <img
+                    src={lineasImg}
+                    className=" w-full absolute bottom-0 right-0"
+                    alt=""
+                />
             </div>
 
             <div className="bg-white flex font-semibold text-xl uppercase text-secondary font-noto-sans rounded-md my-10 overflow-hidden">
-                <Link to='/' className="p-5 hover:bg-primary hover:text-white w-1/2">Write</Link>
-                <Link to='/list-tasks' className="p-5 hover:bg-secondary hover:text-white w-1/2">List</Link>
+                <Link
+                    to="/"
+                    className="p-5 hover:bg-primary hover:text-white w-1/2"
+                >
+                    Write
+                </Link>
+                <Link
+                    to="/list-tasks"
+                    className="p-5 hover:bg-secondary hover:text-white w-1/2"
+                >
+                    List
+                </Link>
             </div>
 
             {children}
-
         </div>
     );
 }
 
 export default Layout;
-
--- src/page/write.tsx
+++ src/page/write.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { useForm } from 'react-hook-form'
 
 import Layout from '../components/layout'
 
+type dataForm = {
+    titulo: String;
+    description: String;
+}
+
 function Write() {
+
+    const { register, handleSubmit, watch } = useForm();
+    const onSubmit = (data: dataForm) => console.log(data);
+
+    console.log(watch("titulo"));
+    console.log(watch("description"));
+
     return (
         <Layout>
             <div
@@ -18,10 +31,10 @@ function Write() {
                     "
             >
                 <h2 className="text-secondary font-semibold text-xl font-noto-sans">Nueva Tarea</h2>
-                <form action="">
-                    <input placeholder="Title" className="appearance-none border-b-2 border-secondary font-noto-sans w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2 mb-2" type="text" />
-                    <textarea placeholder="Description ..." className="w-full h-40 resize-y border border-secondary rounded-md focus:outline-none  py-2 px-3 mt-2 mb-2 font-noto-sans"></textarea>
-                    <button className=" block bg-primary hover:bg-blue-700 text-white font-noto-sans py-2 px-4 rounded-sm mr-0 m-auto">Create Task</button>
+                <form action="" onSubmit={handleSubmit(onSubmit)}>
+                    <input defaultValue="Estudiar para el examen, el curso de Redes." {...register("titulo")} placeholder="Title" className="appearance-none border-b-2 border-secondary font-noto-sans w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2 mb-2" type="text" />
+                    <textarea defaultValue="Basarse en ejemplos y post de cisco." {...register("description")} placeholder="Description ..." className="w-full h-40 resize-y border border-secondary rounded-md focus:outline-none  py-2 px-3 mt-2 mb-2 font-noto-sans"></textarea>
+                    <button type="submit" className=" block bg-primary hover:bg-blue-700 text-white font-noto-sans py-2 px-4 rounded-sm mr-0 m-auto">Create Task</button>
                 </form>
             </div>
         </Layout>
