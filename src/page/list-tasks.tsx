c5c360e5aa9f7e0f8cdb357ed84fe07d3cee1c66
WDavid Calsin <david2c142@gmail.com>
2021-09-01 22:37:13 -0500

    refactor: 💡 added react-hook-form (#4)

--- src/page/list-tasks.tsx
+++ src/page/list-tasks.tsx
@@ -1,35 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import Layout from '../components/layout'
-import ModalTask from "../components/modalTasks";
+import Layout from '../components/layout';
+import ModalTask from '../components/modalTasks';
 
 function ListTasks() {
     return (
         <Layout>
-            {/* <div
-                className="
-                    h-auto
-                    mx-4
-                    w-5/6
-                    rounded-lg
-                    shadow-md
-                    sm:w-1/3 sm:mx-0  
-                    p-5
-                    bg-white
-                    "
-            >
-                Lista 1
-            </div> */}
             <div className="sm:w-3/6 w-full flex flex-col space-y-2">
-
                 <ModalTask />
                 <ModalTask />
                 <ModalTask />
-
             </div>
         </Layout>
-
-
     );
 }
 
